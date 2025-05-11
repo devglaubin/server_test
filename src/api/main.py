d9c011b934b965514fff0fd04b24ac6eb016b4ef
Glaub <devglaub10@gmail.com>
2025-05-11 15:34:42 -0300

    v1.1.1-test

--- src/api/main.py
+++ src/api/main.py
@@ -1,11 +1,12 @@
 import requests
 
 # Data to be sent in the POST request
-# url = "https://181.215.135.132/"
-url = "http://localhost:8000"
+url = "https://181.215.135.132:8000/"
 
+print("pass0")
 # Sending a POST request
 response = requests.get(url)
+print("pass1")
 
 if response.status_code == 200:
     print("Success:", response.status_code)
--- src/server/main.py
+++ src/server/main.py
@@ -10,6 +10,24 @@ class MyHandler(SimpleHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(b"Hello, World!")
+    
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"POST request received!")
+
+    def do_PUT(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"PUT request received!")
+        
+    def do_DELETE(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"DELETE request received!")
 
 # Inicializando o servidor
 server = HTTPServer((host, port), MyHandler)
